b163d3ad7186c42e9662a753faf81bb71a79798f
yuufw <140020609+yuufw@users.noreply.github.com>
2025-12-31 13:37:46 -0300

    Remove unused color definitions from colors.c

--- src/colors.c
+++
@@ -1,5 +0,0 @@
-#include "colors.h"
-
-SDL_Color ORANGE_VINTAGE_CRT = (SDL_Color){255, 164, 81, 255};
-SDL_Color ORANGE_AMBER_CRT = (SDL_Color){255, 183, 76, 255};
-SDL_Color CRT_DARK_BG = (SDL_Color){20, 12, 4, 255};
