8bb34737410836ecd9e5c3ee5a0f0b34b1259c06
yuufw <140020609+yuufw@users.noreply.github.com>
2025-12-31 13:37:53 -0300

    Define color constants in colors.h for improved readability and usage

--- src/colors.h
+++ src/colors.h
@@ -3,8 +3,8 @@
 
 #include <SDL2/SDL.h>
 
-extern SDL_Color ORANGE_VINTAGE_CRT;
-extern SDL_Color CRT_DARK_BG;
-extern SDL_Color ORANGE_AMBER_CRT;
+#define ORANGE_AMBER_CRT (SDL_Color){255, 183, 76, 255}
+#define CRT_DARK_BG (SDL_Color){20, 12, 4, 255}
+#define ORANGE_VINTAGE_CRT (SDL_Color){255, 164, 81, 255}
 
 #endif
