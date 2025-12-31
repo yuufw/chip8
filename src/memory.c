0a5cfcd7fbfc60e2e9bc578a24140d079762f1a9
yuufw <140020609+yuufw@users.noreply.github.com>
2025-12-31 13:33:51 -0300

    Refactor memory_load_rom error handling and improve readability

--- src/memory.c
+++ src/memory.c
@@ -1,6 +1,8 @@
-#include "memory.h"
+#include <errno.h>
 #include <stdio.h>
 #include <string.h>
+
+#include "memory.h"
 
 void memory_init(Memory *m) {
   memset(m->memory, 0, sizeof(m->memory));
@@ -20,37 +22,38 @@ void memory_write(Memory *m, uint16_t addr, uint8_t val) {
 }
 
 int memory_load_rom(Memory *m, const char *path) {
-  FILE *file = fopen(path, "rb");
-  if (!file)
-    return -1;
+    FILE *file = fopen(path, "rb");
+    if (!file) {
+        return -ENOENT;
+    }
 
-  if (fseek(file, 0, SEEK_END) != 0) {
+    if (fseek(file, 0, SEEK_END) != 0) {
+        fclose(file);
+        return -EIO;
+    }
+
+    long size = ftell(file);
+    if (size < 1) {
+        fclose(file);
+        return -EINVAL;
+    }
+
+    if (fseek(file, 0, SEEK_SET) != 0) {
+        fclose(file);
+        return -EIO;
+    }
+
+    if (START_ADDR + size > MEM_SIZE) {
+        fclose(file);
+        return -ENOMEM;
+    }
+
+    size_t read = fread(&m->memory[START_ADDR], 1, (size_t)size, file);
     fclose(file);
-    return -4;
-  }
 
-  long size = ftell(file);
-  if (size < 1) {
-    fclose(file);
-    return -5;
-  }
+    if (read != (size_t)size) {
+        return -EIO;
+    }
 
-  if (fseek(file, 0, SEEK_SET) != 0) {
-    fclose(file);
-    return -6;
-  }
-
-  if (START_ADDR + size > MEM_SIZE) {
-    fclose(file);
-    return -2;
-  }
-
-  size_t read = fread(&m->memory[START_ADDR], 1, size, file);
-  fclose(file);
-
-  if (read != (size_t)size) {
-    return -3;
-  }
-
-  return 0;
+    return 0;
 }
