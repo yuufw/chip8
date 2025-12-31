75f81b1d8119d1a9de2e916bd3c420b5ca33417d
yuufw <140020609+yuufw@users.noreply.github.com>
2025-12-31 15:13:26 -0300

    Refactor input initialization to include memory prefetching for improved performance

--- src/input.c
+++ src/input.c
@@ -1,10 +1,32 @@
 #include "input.h"
+
 #include <string.h>
+#include <stdint.h>
+#include <stddef.h>
+
+#if defined(__x86_64__) || defined(_M_X64)
+#define ARCH_ALIGN 8
+#elif defined(__i386__) || defined(_M_IX86)
+#define ARCH_ALIGN 4
+#else
+#define ARCH_ALIGN sizeof(void *)
+#endif
+
+static inline void prefetch_memory(const void *ptr, size_t len) {
+    uintptr_t addr = (uintptr_t)ptr;
+    uintptr_t end = addr + len;
+
+    for (; addr < end; addr += ARCH_ALIGN) {
+        volatile uint8_t tmp = *(uint8_t *)addr;
+        (void)tmp;
+    }
+}
 
 void input_init(Input *in) {
-  memset(in->keys, 0, sizeof in->keys);
-  in->waiting_for_key = -1;
-  in->exit_signal = 0;
+    prefetch_memory(in, sizeof(*in));
+    memset(in->keys, 0, sizeof in->keys);
+    in->waiting_for_key = -1;
+    in->exit_signal = 0;
 }
 
 void input_set_key(Input *in, uint8_t key, uint8_t pressed) {
